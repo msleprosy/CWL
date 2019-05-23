edc24f527d92b3a33ef8f70f55a715df86427f47
Veronika Baranova <Veronika_Baranova@epam.com>
2019-05-24 02:16:11 +0300

    Creates UserDao and Storage

---
+++ src/main/java/com/epam/cwlhub/dao/UserDao.java
@@ -0,0 +1,31 @@
+package com.epam.cwlhub.dao;
+
+import com.epam.cwlhub.entities.user.UserEntity;
+
+import java.util.Optional;
+
+import static com.epam.cwlhub.storage.Storage.usersList;
+
+/**
+ * Created by veronika on 24.05.2019.
+ */
+public class UserDao {
+
+    public void add(UserEntity user) {
+        usersList.add(user);
+    }
+
+    public void deleteById(long id) {
+        findUserById(id).map(user -> usersList.remove(user));
+    }
+
+    public void update(UserEntity user){}
+
+    public void printAll() {
+        usersList.forEach(user -> System.out.println(user));
+    }
+
+    private Optional<UserEntity> findUserById(long userId) {
+        return usersList.stream().filter(user -> userId == user.getId()).findAny();
+    }
+}
---
+++ src/main/java/com/epam/cwlhub/storage/Storage.java
@@ -0,0 +1,13 @@
+package com.epam.cwlhub.storage;
+
+import com.epam.cwlhub.entities.user.UserEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by veronika on 24.05.2019.
+ */
+public class Storage {
+    public static List<UserEntity> usersList = new ArrayList<>();
+}
