421e8ddcca93182144022bbaec35ff4834e47b4b
Veronika Baranova <Veronika_Baranova@epam.com>
2019-05-28 10:55:33 +0300

    Adds readAllLines method

--- src/main/java/com/epam/cwlhub/storage/initor/DBInitor.java
+++ src/main/java/com/epam/cwlhub/storage/initor/DBInitor.java
@@ -2,11 +2,12 @@ package com.epam.cwlhub.storage.initor;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
-import java.io.File;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 public class DBInitor {
     private static final String DDL_SCRIPT_PATH = "database/ddl/create_tables.sql";
@@ -31,17 +32,15 @@ public class DBInitor {
 
     private void createDataBaseStructure() throws Exception {
         ClassLoader classLoader = this.getClass().getClassLoader();
-        File file = new File(classLoader.getResource(DDL_SCRIPT_PATH).getFile());
-        String content = new String(Files.readAllBytes(file.toPath()));
+        List<String> ddl = Files.readAllLines(Paths.get(classLoader.getResource(DDL_SCRIPT_PATH).toURI()));
         Statement statement = getDBConnection().createStatement();
-        statement.execute(content);
+        statement.execute(String.join("", ddl));
     }
 
     private void fillDataBaseWithData() throws Exception {
         ClassLoader classLoader = this.getClass().getClassLoader();
-        File file = new File(classLoader.getResource(DML_SCRIPT_PATH).getFile());
-        String content = new String(Files.readAllBytes(file.toPath()));
+        List<String> dml = Files.readAllLines(Paths.get(classLoader.getResource(DML_SCRIPT_PATH).toURI()));
         Statement statement = getDBConnection().createStatement();
-        statement.executeUpdate(content);
+        statement.executeUpdate(String.join("", dml));
     }
 }
