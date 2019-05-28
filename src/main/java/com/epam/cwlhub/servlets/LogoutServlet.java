539c174b427fc8d4c08bf7c1cedee18961a4de27
soignera <askasoignera@gmail.com>
2019-05-28 21:08:14 +0300

    CWL-17-Create_LogoutServlet(!1)

---
+++ src/main/java/com/epam/cwlhub/servlets/LogoutServlet.java
@@ -0,0 +1,30 @@
+package com.epam.cwlhub.servlets;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+//@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public LogoutServlet() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.removeAttribute("user");
+
+            RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");
+            dispatcher.forward(request, response);
+        }
+    }
+}
+
