5dd7c9e4d79115c7d112019f25e510d59a243e9f
Elena_Neverova <Elena_Neverova@epam.com>
2019-05-30 02:01:18 +0400

    CWL-14-Create_SnippetDao Removes unnecessary exceptions

--- src/main/java/com/epam/cwlhub/exceptions/checked/SqlError.java
+++
@@ -1,9 +0,0 @@
-package com.epam.cwlhub.exceptions.checked;
-
-import com.epam.cwlhub.exceptions.ExceptionMeta;
-
-public class SqlError extends CWLHubCheckedException {
-    public SqlError(ExceptionMeta exceptionMeta, Throwable cause) {
-        super(exceptionMeta, cause);
-    }
-}
