f0992352ad0c6d7c5e5d64c75fd57d7fc1d206f3
msleprosy <msleprosy@yandex.ru>
2019-05-30 23:27:16 +0300

    test commit

--- src/main/java/com/epam/cwlhub/dao/user/impl/UserDaoImpl.java
+++ src/main/java/com/epam/cwlhub/dao/user/impl/UserDaoImpl.java
@@ -5,6 +5,7 @@ import com.epam.cwlhub.entities.user.UserType;
 import com.epam.cwlhub.storage.dbconnection.DBConnection;
 import com.epam.cwlhub.storage.dbconnection.DBConnector;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -17,15 +18,25 @@ public class UserDaoImpl {
     private static volatile UserDaoImpl INSTANCE;
 
     private static final String INSERT_USER_SQL_STATEMENT = "INSERT INTO users (firstname, lastname, email, "
-                                                            + " password, banned, user_type)"
-                                                            + "Values (?, ?, ?, ?, ?, ?)";
+                                                                            + " password, banned, user_type)"
+                                                                            + "Values (?, ?, ?, ?, ?, ?)";
+    private static final String SELECT_USER_BY_ID_SQL_STATEMENT = "SELECT * FROM users WHERE user_id = ?";
+    private static final String SELECT_USER_SQL_STATEMENT = "SELECT * FROM users WHERE";
+    private static final String SELECT_USER_BY_GROUP_ID_SQL_STATEMENT = SELECT_USER_SQL_STATEMENT + "group_id = ?";
+    private static final String SELECT_USER_BY_EMAIL_SQL_STATEMENT = SELECT_USER_SQL_STATEMENT + "email = ?";
+    private static final String SELECT_ALL_USERS_SQL_STATEMENT = "SELECT * FROM users";
+    private static final String DELETE_USER_SQL_STATEMENT = "DELETE FROM users WHERE";
+    private static final String DELETE_USER_BY_ID_SQL_STATEMENT = DELETE_USER_SQL_STATEMENT + "user_id = ?";
+    private static final String DELETE_USER_BY_EMAIL_SQL_STATEMENT = DELETE_USER_SQL_STATEMENT + "email = ?";
+    private static final String UPDATE_USER_SQL_STATEMENT = "UPDATE users SET firstname = ?, lastname = ?, "
+                                                                      + "email = ?, password = ?, banned = ?";
 
-    public static UserDaoImpl getInstance(){
+    public static UserDaoImpl getInstance() {
         UserDaoImpl localInstance = INSTANCE;
-        if (localInstance == null){
-            synchronized (UserDaoImpl.class){
+        if (localInstance == null) {
+            synchronized (UserDaoImpl.class) {
                 localInstance = INSTANCE;
-                if (localInstance == null){
+                if (localInstance == null) {
                     INSTANCE = localInstance = new UserDaoImpl();
                 }
             }
@@ -34,102 +45,141 @@ public class UserDaoImpl {
     }
 
     public UserEntity insert(UserEntity user) {
-        String userType = String.valueOf(UserType.SIMPLE_USER);
-        String sql = "INSERT INTO users (firstname, lastname, email, password, banned, user_type) Values (?, ?, ?, ?, ?, ?)";
-        try {
-            PreparedStatement preparedStatement = dbConnection.getDBConnection().prepareStatement(sql);
-            preparedStatement.setString(1, user.getFirstName());
-            preparedStatement.setString(2, user.getLastName());
-            preparedStatement.setString(3, user.getEmail());
-            preparedStatement.setString(4, user.getPassword());
-            preparedStatement.setBoolean(5, user.isBanned());
-            preparedStatement.setString(6, userType);
-            int rows = preparedStatement.executeUpdate();
-        } catch (Exception e) {
-            e.printStackTrace();
+        try (Connection connection = dbConnection.getDBConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USER_SQL_STATEMENT,
+                     PreparedStatement.RETURN_GENERATED_KEYS)) {
+            appendPreparedStatementParametersToInsertUser(preparedStatement, user);
+            preparedStatement.executeUpdate();
+            try (ResultSet generatedId = preparedStatement.getGeneratedKeys()) {
+                if (generatedId.next()) {
+                    user.setId(generatedId.getLong(1));
+                }
+            }
+            return user;
+        } catch (Exception ex) {
+            throw new UserException("Can't insert new user", ex);
         }
     }
 
     public Optional<UserEntity> findById(long id) throws Exception {
-        String sql = "SELECT * FROM users WHERE user_id = ?";
-        PreparedStatement preparedStatement = dbConnection.getDBConnection().prepareStatement(sql);
-        preparedStatement.setLong(1, id);
-        ResultSet rs = preparedStatement.executeQuery();
-        if (rs.next())
-            return Optional.of(UserMapper.mapUser(rs));
-        else
-            return Optional.empty();
+        try (Connection connection = dbConnection.getDBConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_USER_BY_ID_SQL_STATEMENT)){
+            preparedStatement.setLong(1, id);
+            ResultSet rs = preparedStatement.executeQuery();
+            if (rs.next()){
+                return Optional.ofNullable(mapUser(rs));
+            } else {
+                return Optional.empty();
+            }
+        } catch (Exception ex) {
+            throw new UserException("Can't find the user with id" + id, ex);
+        }
     }
 
     public List<UserEntity> findByGroupId(long id) {
+        try (Connection connection = dbConnection.getDBConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_USER_BY_GROUP_ID_SQL_STATEMENT)){
+            preparedStatement.setLong(1, id);
+            ResultSet rs = preparedStatement.executeQuery();
+            return getUsersFromResultSet(rs);
+        } catch (Exception ex) {
+            throw new UserException("Can't find the user with id" + id, ex);
+        }
     }
 
     public List<UserEntity> findAll(long id) {
+        try (Connection connection = dbConnection.getDBConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_USERS_SQL_STATEMENT)){
+            preparedStatement.setLong(1, id);
+            ResultSet rs = preparedStatement.executeQuery();
+            return getUsersFromResultSet(rs);
+        } catch (Exception ex) {
+            throw new UserException("Can't find any users", ex);
+        }
+    }
+
+    public Optional<UserEntity> findByEmail(String email) throws Exception {
+        try (Connection connection = dbConnection.getDBConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_USER_BY_EMAIL_SQL_STATEMENT)){
+            preparedStatement.setString(1, email);
+            ResultSet rs = preparedStatement.executeQuery();
+            if (rs.next()){
+                return Optional.ofNullable(mapUser(rs));
+            } else {
+                return Optional.empty();
+            }
+        } catch (Exception ex) {
+            throw new UserException("Can't find the user with email" + email, ex);
+        }
+    }
+
+    public void deleteById(long id) {
+        try (Connection connection = dbConnection.getDBConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(DELETE_USER_BY_ID_SQL_STATEMENT)) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+        } catch (Exception ex) {
+            throw new UserException("Can't delete the user with id" + id, ex);
+        }
+    }
+
+    public void deleteByEmail(String email) {
+        try (Connection connection = dbConnection.getDBConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(DELETE_USER_BY_ID_SQL_STATEMENT)) {
+            preparedStatement.setString(1, email);
+            preparedStatement.executeUpdate();
+        } catch (Exception ex) {
+            throw new UserException("Can't delete the user with email" + email, ex);
+        }
+    }
+
+    public void update(UserEntity user) {
+        try (Connection connection = dbConnection.getDBConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_USER_SQL_STATEMENT)) {
+            appendPreparedStatementParametersToIUpdateUser(preparedStatement, user);
+             preparedStatement.executeUpdate();
+        } catch (Exception ex) {
+            throw new UserException("Can't update the user with id" + user.getId(), ex);
+        }
     }
 
     private List<UserEntity> getUsersFromResultSet(ResultSet rs) throws SQLException {
     }
 
-    private void appendPreparedStatementParametersToInsetUser(PreparedStatement ps, UserEntity userEntity) throws SQLException {
+    private void appendPreparedStatementParametersToInsertUser(PreparedStatement preparedStatement, UserEntity user) throws SQLException {
+        String userType = String.valueOf(UserType.SIMPLE_USER);
+        preparedStatement.setString(1, user.getFirstName());
+        preparedStatement.setString(2, user.getLastName());
+        preparedStatement.setString(3, user.getEmail());
+        preparedStatement.setString(4, user.getPassword());
+        preparedStatement.setBoolean(5, user.isBanned());
+        preparedStatement.setString(6, userType);
+    }
+
+    private void appendPreparedStatementParametersToIUpdateUser(PreparedStatement preparedStatement, UserEntity user) throws SQLException {
+        preparedStatement.setString(1, user.getFirstName());
+        preparedStatement.setString(2, user.getLastName());
+        preparedStatement.setString(3, user.getEmail());
+        preparedStatement.setString(4, user.getPassword());
+        preparedStatement.setBoolean(5, user.isBanned());
     }
 
     private UserEntity mapUser(ResultSet rs) {
-    }
-
-    private void appendPreparedStatementParametersToUpdateUser(PreparedStatement ps, UserEntity userEntity) throws SQLException {
-    }
-
-    public Optional<UserEntity> findByEmail(String email) throws Exception {
-        String sql = "SELECT * FROM users WHERE email = ?";
-        PreparedStatement preparedStatement = dbConnection.getDBConnection().prepareStatement(sql);
-        preparedStatement.setString(1, email);
-        ResultSet rs = preparedStatement.executeQuery();
-        if (rs.next())
-            return Optional.of(UserMapper.mapUser(rs));
-        else
-            return Optional.empty();
-    }
-
-    public void deleteById(long id) {
-        try {
-            Optional<UserEntity> found = findById(id);
-            if (found.isPresent()) {
-                String sql = "DELETE FROM users WHERE user_id = ?";
-                PreparedStatement preparedStatement = dbConnection.getDBConnection().prepareStatement(sql);
-                preparedStatement.setLong(1, id);
-                int rows = preparedStatement.executeUpdate();
+        UserEntity user = new UserEntity();
+        try{
+            long id = rs.getLong("user_id");
+            if (id != 1) {
+                user.setId(id);
+                user.setLastName(rs.getString("lastname"));
+                user.setFirstName(rs.getString("firstname"));
+                user.setEmail(rs.getString("email"));
+                user.setBanned(rs.getBoolean("banned"));
+                user.setUserType(UserType.SIMPLE_USER);
             }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    public void deleteByEmail(String email) {
-        try {
-            Optional<UserEntity> found = findByEmail(email);
-            if (found.isPresent()) {
-                String sql = "DELETE FROM users WHERE email = ?";
-                PreparedStatement preparedStatement = dbConnection.getDBConnection().prepareStatement(sql);
-                preparedStatement.setString(1, email);
-                int rows = preparedStatement.executeUpdate();
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    public void update(UserEntity user) {
-        String sql = "UPDATE users SET firstname = ?, lastname = ?, email = ?, password = ?, banned = ?";
-        try {
-            PreparedStatement preparedStatement = dbConnection.getDBConnection().prepareStatement(sql);
-            preparedStatement.setString(1, user.getFirstName());
-            preparedStatement.setString(2, user.getLastName());
-            preparedStatement.setString(3, user.getEmail());
-            preparedStatement.setString(4, user.getPassword());
-            preparedStatement.setBoolean(5, user.isBanned());
-            int rows = preparedStatement.executeUpdate();
-        } catch (Exception e) {
-            e.printStackTrace();
+            return user;
+        }catch (SQLException ex){
+            throw new UserException("can't map resultset to the user entity", ex);
         }
     }
 }
+
