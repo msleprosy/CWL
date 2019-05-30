e31fc42180d903f53bcc7ce238a5c7a0372ad1da
Elena_Neverova <Elena_Neverova@epam.com>
2019-05-30 12:12:14 +0400

    CWL-14-Create_SnippetDao Changes SnippetException constructor

--- src/main/java/com/epam/cwlhub/dao/impl/SnippetDaoImpl.java
+++ src/main/java/com/epam/cwlhub/dao/impl/SnippetDaoImpl.java
@@ -3,6 +3,8 @@ package com.epam.cwlhub.dao.impl;
 import com.epam.cwlhub.dao.SnippetDao;
 import com.epam.cwlhub.entities.snippet.Snippet;
 import com.epam.cwlhub.exceptions.unchecked.SnippetException;
+import com.epam.cwlhub.storage.dbconnection.DBConnection;
+import com.epam.cwlhub.storage.dbconnection.DBConnector;
 
 import java.sql.*;
 import java.sql.Date;
@@ -48,7 +50,8 @@ public class SnippetDaoImpl implements SnippetDao {
 
     @Override
     public Snippet insert(Snippet snippet) {
-        try (Connection connection = getDBConnectorInstance().getDBConnection();
+        DBConnection dbConnection = DBConnector.getDBConnectorInstance();
+        try (Connection connection = dbConnection.getDBConnection();
              PreparedStatement ps = connection.prepareStatement(INSERT_SNIPPET_SQL_STATEMENT,
                                                                 PreparedStatement.RETURN_GENERATED_KEYS)) {
             appendPreparedStatementParametersToInsertSnippet(ps, snippet);
--- src/main/java/com/epam/cwlhub/exceptions/unchecked/SnippetException.java
+++ src/main/java/com/epam/cwlhub/exceptions/unchecked/SnippetException.java
@@ -2,7 +2,6 @@ package com.epam.cwlhub.exceptions.unchecked;
 
 public class SnippetException extends RuntimeException {
     public SnippetException(String message, Throwable cause) {
-        super(message);
-        initCause(cause);
+        super(message, cause);
     }
 }
