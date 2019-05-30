d561e0b3c63b33be7a23d85da5a75d30fbbc517d
soignera <askasoignera@gmail.com>
2019-05-30 19:34:12 +0300

    CWL-16-Create_LoginServlet(!3) Change LoginServlet Replace login.jsp with loginView.jsp

--- src/main/java/com/epam/cwlhub/servlets/LoginServlet.java
+++ src/main/java/com/epam/cwlhub/servlets/LoginServlet.java
@@ -2,6 +2,7 @@ package com.epam.cwlhub.servlets;
 
 import com.epam.cwlhub.entities.user.UserEntity;
 
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,78 +12,71 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.*;
 
-//@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
+    private static final String ACCEPT_REMEMBERME = "Y";
+    private static final String ERROR = "errorString";
+    private static final String USER = "user";
 
-    public LoginServlet() {
-        super();
-    }
-    public UserEntity checkLogin(String email, String password) throws SQLException
-    {
-        String jdbcURL = "jdbc:postgresql://ECSC00A04EEC.epam.com:5432/postgres";
-        String dbUser = "postgres";
-        String dbPassword = "CWLHubHardPassword228";
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
+        RequestDispatcher dispatcher
+                = this.getServletContext().getRequestDispatcher(UrlAttrs.LOGIN_URL);
 
-        //Class.forName("org.postgresql.Driver");
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        Connection connection = DriverManager.getConnection(jdbcURL, dbUser, dbPassword);
-        String sql = "SELECT * FROM users WHERE email = ? and password = ?";
-        System.out.println(sql);
-        //  String sql = "SELECT * FROM persons";
+        dispatcher.forward(request, response);
 
-        PreparedStatement statement = connection.prepareStatement(sql);
-        statement.setString(1, email);
-        statement.setString(2, password);
-
-        ResultSet result = statement.executeQuery();
-
-        UserEntity user = null;
-
-
-
-        if (result.next()) {
-            user = new UserEntity();
-            user.setLastName(result.getString("lastName"));
-            user.setEmail(email);
-        }
-
-        connection.close();
-
-        return user;
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String email = request.getParameter("email");
-        String password = request.getParameter("password");
+        String email = request.getParameter(UserAttrs.EMAIL_PARAMETER).trim();
+        String password = request.getParameter(UserAttrs.PASSWORD_PARAMETER).trim();
+        String rememberMeStr = request.getParameter(UserAttrs.REMEMBERME_PARAMETER);
+        boolean remember = ACCEPT_REMEMBERME.equals(rememberMeStr);
 
-        try {
-            UserEntity user = checkLogin(email, password);
-            String destPage = "login.jsp";
-
-            if (user != null) {
-                HttpSession session = request.getSession();
-                session.setAttribute("user", user);
-                destPage = "home.jsp";
-            } else {
-                String message = "Invalid email/password";
-                request.setAttribute("message: ", message);
-                response.getWriter().println("message: " + message);
+        UserEntity user = null;
+        boolean hasError = false;
+        String errorString = null;
+        if (email == null || password == null || email.length() == 0 || password.length() == 0) {
+            hasError = true;
+            errorString = UserAttrs.AUTHORIZATION_ERROR;
+        } else {
+            Connection conn = MyUtils.getStoredConnection(request);
+            try {
+                user = DBUtils.findUser(conn, email, password);
+                if (user == null) {
+                    hasError = true;
+                    errorString = UserAttrs.LOGIN_ERROR;
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+                hasError = true;
+                errorString = e.getMessage();
             }
-
-            RequestDispatcher dispatcher = request.getRequestDispatcher(destPage);
-            dispatcher.forward(request, response);
-
         }
-        catch (SQLException ex ) {
-            throw new ServletException(ex);
+        if (hasError) {
+            user = new UserEntity();
+            user.setEmail(email);
+            user.setPassword(password);
+            request.setAttribute(ERROR, errorString);
+            request.setAttribute(USER, user);
+            RequestDispatcher dispatcher //
+                    = this.getServletContext().getRequestDispatcher(UrlAttrs.LOGIN_URL);
+
+            dispatcher.forward(request, response);
+        } else {
+            HttpSession session = request.getSession();
+            MyUtils.storeLoginedUser(session, user);
+
+            if (remember) {
+                MyUtils.storeUserCookie(response, user);
+            } else {
+                MyUtils.deleteUserCookie(response);
+            }
+            response.sendRedirect(request.getContextPath() + UrlAttrs.USERINFO_URL);
         }
     }
-
 }
+
