9344500178de4acc9a9e0fe2448d19eb3d7565b1
Nikolai Slobodeniuk <Nikolai_Slobodeniuk@epam.com>
2019-05-30 19:05:16 +0300

    CWL-36 - Created GroupService interface

---
+++ src/main/java/com/epam/cwlhub/services/group/GroupService.java
@@ -0,0 +1,7 @@
+package com.epam.cwlhub.services.group;
+
+import com.epam.cwlhub.entities.group.Group;
+import com.epam.cwlhub.services.BaseService;
+
+public interface GroupService extends BaseService<Group> {
+}
