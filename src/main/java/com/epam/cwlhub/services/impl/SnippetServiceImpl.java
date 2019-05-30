97619e4cc25075dd9db1ebc1275bc5fa6267741d
Elena_Neverova <Elena_Neverova@epam.com>
2019-05-30 19:16:33 +0400

    CWL-13-Create_SnippetDao Creates SnippetService

---
+++ src/main/java/com/epam/cwlhub/services/impl/SnippetServiceImpl.java
@@ -0,0 +1,72 @@
+package com.epam.cwlhub.services.impl;
+
+import com.epam.cwlhub.dao.SnippetDao;
+import com.epam.cwlhub.dao.impl.SnippetDaoImpl;
+import com.epam.cwlhub.entities.snippet.Snippet;
+import com.epam.cwlhub.services.SnippetService;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+public class SnippetServiceImpl implements SnippetService {
+    private final SnippetDao snippetDao = SnippetDaoImpl.getInstance();
+
+    private static volatile SnippetServiceImpl INSTANCE;
+
+    public static SnippetServiceImpl getInstance() {
+        SnippetServiceImpl localInstance = INSTANCE;
+        if (localInstance == null) {
+            synchronized (SnippetServiceImpl.class) {
+                localInstance = INSTANCE;
+                if (localInstance == null) {
+                    INSTANCE = localInstance = new SnippetServiceImpl();
+                }
+            }
+        }
+        return localInstance;
+    }
+
+    @Override
+    public Snippet insert(Snippet snippet) {
+        if (snippet != null) {
+            snippetDao.insert(snippet);
+        }
+        return snippet;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        if (id != null) {
+            snippetDao.deleteById(id);
+        }
+    }
+
+    @Override
+    public Optional<Snippet> findById(Long id) {
+        if (id != null)
+            return snippetDao.findById(id);
+
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Snippet> findByGroupId(Long id) {
+        if (id != null) {
+            return snippetDao.findByGroupId(id);
+        }
+        return Collections.emptyList();
+    }
+
+    @Override
+    public void update(Snippet snippet) {
+        if (snippet != null) {
+            snippetDao.update(snippet);
+        }
+    }
+
+    @Override
+    public List<Snippet> findAll() {
+        return snippetDao.findAll();
+    }
+}
