3da13a045480f0436a5a62bdd3751827e8a63251
msleprosy <msleprosy@yandex.ru>
2019-05-30 16:35:49 +0300

    Rewrites properties path

--- src/main/java/com/epam/cwlhub/storage/dbconnection/DBConnector.java
+++ src/main/java/com/epam/cwlhub/storage/dbconnection/DBConnector.java
@@ -4,6 +4,7 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.util.Properties;
 
@@ -12,17 +13,18 @@ public class DBConnector implements DBConnection {
     private static final String HOST = "db.host";
     private static final String LOGIN = "db.login";
     private static final String PASSWORD = "db.password";
-    private static final String PROPERTIES_PATH = "src/main/resources/database/config/config.properties";
+    private static final String PROPERTIES_PATH = "database/config/config.properties";
     private static DBConnector dbConnector = null;
     private static ComboPooledDataSource cpds;
+
+    InputStream fis = this.getClass().getClassLoader().getResourceAsStream(PROPERTIES_PATH);
 
     static{
         cpds = new ComboPooledDataSource();
         Properties property = new Properties();
-        FileInputStream fis;
         try {
-            fis = new FileInputStream(PROPERTIES_PATH);
-            property.load(fis);
+
+            property.load(new DBConnector().fis);
             cpds.setJdbcUrl(property.getProperty(HOST));
             cpds.setUser(property.getProperty(LOGIN));
             cpds.setPassword(property.getProperty(PASSWORD));
