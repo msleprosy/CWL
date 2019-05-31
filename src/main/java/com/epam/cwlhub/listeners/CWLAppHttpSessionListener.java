55c04e1ca60661dcffbdf8c04ed0fa178b518542
Liz <skystar-el@yandex.ru>
2019-05-31 20:47:59 +0300

    Updates CWLAppHttpSessionListener

--- src/main/java/com/epam/cwlhub/listeners/CWLAppHttpSessionListener.java
+++ src/main/java/com/epam/cwlhub/listeners/CWLAppHttpSessionListener.java
@@ -3,12 +3,16 @@ package com.epam.cwlhub.listeners;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionListener;
 import javax.servlet.http.HttpSessionEvent;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class CWLAppHttpSessionListener implements HttpSessionListener {
 
     @Override
     public void sessionCreated(HttpSessionEvent httpSessionEvent) {
         HttpSession session = httpSessionEvent.getSession();
+        Map<String, Long> userSessionData = new ConcurrentHashMap<>();
+        session.setAttribute("UserSessionData", userSessionData);
 
     }
 }
