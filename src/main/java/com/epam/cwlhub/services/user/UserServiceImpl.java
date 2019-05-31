0c863e0250b4d8c2412a47c7ce418169433892f1
msleprosy <msleprosy@yandex.ru>
2019-05-31 03:47:47 +0300

    Adds several methods from dao

--- src/main/java/com/epam/cwlhub/services/user/UserServiceImpl.java
+++ src/main/java/com/epam/cwlhub/services/user/UserServiceImpl.java
@@ -8,38 +8,68 @@ import java.util.Optional;
 public class UserServiceImpl implements UserService {
     private final UserDao userDao = UserDaoImpl.getInstance();
 
+    private static volatile UserServiceImpl INSTANCE;
+
+    public static UserServiceImpl getInstance() {
+        UserServiceImpl localInstance = INSTANCE;
+        if (localInstance == null) {
+            synchronized (UserServiceImpl.class) {
+                localInstance = INSTANCE;
+                if (localInstance == null) {
+                    INSTANCE = localInstance = new UserServiceImpl();
+                }
+            }
+        }
+        return localInstance;
+    }
+
     @Override
     public Optional<UserEntity> findByEmail(String email) {
+        if (email != null){
+            return userDao.findByEmail(email);
+        }
         return Optional.empty();
     }
 
     @Override
     public void deleteByEmail(String email) {
-
+        if (email != null) {
+            userDao.deleteByEmail();
+        }
     }
 
     @Override
-    public UserEntity insert(UserEntity entity) {
-        return null;
+    public UserEntity insert(UserEntity user) {
+        if (user != null){
+            userDao.insert(user);
+        }
+        return user;
     }
 
     @Override
     public void deleteById(Long id) {
-
+        if (id != null) {
+            userDao.deleteById();
+        }
     }
 
     @Override
     public Optional<UserEntity> findById(Long id) {
+        if (id != null){
+            return userDao.findById(id);
+        }
         return Optional.empty();
     }
 
     @Override
-    public void update(UserEntity entity) {
-
+    public void update(UserEntity user) {
+        if (user != null){
+            userDao.update(user);
+        }
     }
 
     @Override
     public List<UserEntity> findAll() {
-        return null;
+        return userDao.findAll();
     }
 }
