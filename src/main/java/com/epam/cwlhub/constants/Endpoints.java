0c33e2ed5369ee090e8cd9d900a9ebfde6b90325
soignera <askasoignera@gmail.com>
2019-06-01 15:32:01 +0300

    CWL-17-Create_LogoutServlet

---
+++ src/main/java/com/epam/cwlhub/constants/Endpoints.java
@@ -0,0 +1,8 @@
+package com.epam.cwlhub.constants;
+
+public class Endpoints {
+    public static final String LOGIN_PAGE = "/loginView.jsp";
+    public static final String USERINFO_URL = "/userInfo";
+    public static final String LOGIN_URL = "/login";
+    public static final String USERINFOVIEW_PAGE = "/UserInfoView.jsp";
+}
