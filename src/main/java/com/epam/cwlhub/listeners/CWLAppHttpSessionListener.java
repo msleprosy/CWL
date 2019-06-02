00852fe386d1e2a2b304e1d638b138a15a87e78a
Elizaveta Lutsenko <Elizaveta_Lutsenko@epam.com>
2019-06-02 15:44:52 +0300

    Updates CWLAppHttpSessionListener class

--- src/main/java/com/epam/cwlhub/listeners/CWLAppHttpSessionListener.java
+++ src/main/java/com/epam/cwlhub/listeners/CWLAppHttpSessionListener.java
@@ -8,11 +8,31 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class CWLAppHttpSessionListener implements HttpSessionListener {
 
+    private Map<Long, HttpSession> userSessionData = new ConcurrentHashMap<>();
+
+    public Map<Long, HttpSession> getUserSessionData() {
+        return userSessionData;
+    }
+
     @Override
     public void sessionCreated(HttpSessionEvent httpSessionEvent) {
         HttpSession session = httpSessionEvent.getSession();
-        Map<String, Long> userSessionData = new ConcurrentHashMap<>();
-        session.setAttribute("UserSessionData", userSessionData);
+        Long userId = (Long) session.getAttribute("loginedUser.getId()");
+        if (userId != null) {
+            if (userSessionData.containsKey(userId)) {
+                userSessionData.get(userId).invalidate();
+            }
+            userSessionData.put(userId, session);
+        }
+    }
 
+    @Override
+    public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
+        HttpSession session = httpSessionEvent.getSession();
+        Long userId = (Long) session.getAttribute("loginedUser.getId()");
+        if (userId != null) {
+            userSessionData.remove(userId);
+        }
     }
 }
+
