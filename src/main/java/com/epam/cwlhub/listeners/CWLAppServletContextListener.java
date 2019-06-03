658d05b8e7f444b75dd60a66789cb58328de8157
Elizaveta Lutsenko <Elizaveta_Lutsenko@epam.com>
2019-06-03 19:20:52 +0300

    Adds minor changes to CWLAppServletContextListener

--- src/main/java/com/epam/cwlhub/listeners/CWLAppServletContextListener.java
+++ src/main/java/com/epam/cwlhub/listeners/CWLAppServletContextListener.java
@@ -3,18 +3,18 @@ package com.epam.cwlhub.listeners;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import javax.servlet.http.HttpSession;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class CWLAppServletContextListener implements ServletContextListener{
 
+    public static final String USER_SESSION_DATA = "UserSessionData";
+
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         Map<String, Long> userSessionData = new ConcurrentHashMap<>();
         ServletContext context = servletContextEvent.getServletContext();
-        context.setAttribute("UserSessionData", userSessionData);
-
+        context.setAttribute(USER_SESSION_DATA, userSessionData);
     }
 }
 
