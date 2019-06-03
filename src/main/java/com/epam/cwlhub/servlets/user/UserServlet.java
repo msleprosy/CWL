6c925e729827ea329041b5f3a996dc1b5d10aecb
msleprosy <msleprosy@yandex.ru>
2019-06-03 19:43:08 +0300

    Adds initial version of user servlet

---
+++ src/main/java/com/epam/cwlhub/servlets/user/UserServlet.java
@@ -0,0 +1,18 @@
+package com.epam.cwlhub.servlets.user;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class UserServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        PrintWriter out = resp.getWriter();
+        out.print("<h1>Hello Servlet</h1>");
+    }
+}
