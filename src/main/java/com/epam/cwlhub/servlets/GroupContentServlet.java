e47f341e12eac1635ace09a95fd60cc43fd959d5
Elena_Neverova <Elena_Neverova@epam.com>
2019-06-04 10:03:44 +0400

    CWL-42-Create_CommonSnippetsServlet Adds representation of user groups on pages

--- src/main/java/com/epam/cwlhub/servlets/GroupContentServlet.java
+++ src/main/java/com/epam/cwlhub/servlets/GroupContentServlet.java
@@ -1,8 +1,13 @@
 package com.epam.cwlhub.servlets;
 
+import com.epam.cwlhub.entities.group.Group;
 import com.epam.cwlhub.entities.snippet.Snippet;
+import com.epam.cwlhub.entities.user.UserEntity;
+import com.epam.cwlhub.services.GroupService;
 import com.epam.cwlhub.services.SnippetService;
+import com.epam.cwlhub.services.impl.GroupServiceImpl;
 import com.epam.cwlhub.services.impl.SnippetServiceImpl;
+import com.epam.cwlhub.services.impl.UserServiceImpl;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -12,22 +17,36 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
+import static com.epam.cwlhub.constants.Endpoints.GROUP;
 import static com.epam.cwlhub.constants.Endpoints.GROUP_URL;
+import static com.epam.cwlhub.listeners.CWLAppServletContextListener.USER_SESSION_DATA;
 
 @WebServlet(name = "SnippetsList", urlPatterns = GROUP_URL)
 public class GroupContentServlet extends HttpServlet {
     private final SnippetService snippetService = SnippetServiceImpl.getInstance();
+    private final GroupService groupService = GroupServiceImpl.getInstance();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if (req.getParameterMap().containsKey("id")) {
-            Long id = Long.parseLong(req.getParameter("id"));
-            List<Snippet> snippets = snippetService.findByGroupId(id);
-            req.setAttribute("snippets", snippets);
+        Long userId = ((Map<String, Long>) req.getServletContext().getAttribute(USER_SESSION_DATA))
+                .get(req.getSession().getId());
+        Optional<UserEntity> receivedUser = UserServiceImpl.getInstance().findById(userId);
 
-            RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher(GROUP_URL);
-            dispatcher.forward(req, resp);
+        if (receivedUser.isPresent()) {
+            if (req.getParameterMap().containsKey("id")) {
+                Long groupId = Long.parseLong(req.getParameter("id"));
+                List<Snippet> snippets = snippetService.findByGroupId(groupId);
+                req.setAttribute("snippets", snippets);
+
+                List<Group> userGroups = groupService.findUsersGroups(receivedUser.get().getId());
+                req.setAttribute("userGroups", userGroups);
+
+                RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher(GROUP);
+                dispatcher.forward(req, resp);
+            }
         }
     }
 }
--- src/main/java/com/epam/cwlhub/servlets/HomeServlet.java
+++ src/main/java/com/epam/cwlhub/servlets/HomeServlet.java
@@ -1,8 +1,13 @@
 package com.epam.cwlhub.servlets;
 
+import com.epam.cwlhub.entities.group.Group;
 import com.epam.cwlhub.entities.snippet.Snippet;
+import com.epam.cwlhub.entities.user.UserEntity;
+import com.epam.cwlhub.services.GroupService;
 import com.epam.cwlhub.services.SnippetService;
+import com.epam.cwlhub.services.impl.GroupServiceImpl;
 import com.epam.cwlhub.services.impl.SnippetServiceImpl;
+import com.epam.cwlhub.services.impl.UserServiceImpl;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -13,20 +18,33 @@ import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import static com.epam.cwlhub.constants.Endpoints.HOME;
 import static com.epam.cwlhub.constants.Endpoints.HOME_URL;
+import static com.epam.cwlhub.listeners.CWLAppServletContextListener.USER_SESSION_DATA;
 
 @WebServlet(name = "HomeServlet", urlPatterns = HOME_URL)
 public class HomeServlet extends HttpServlet {
     private final SnippetService snippetService = SnippetServiceImpl.getInstance();
+    private final GroupService groupService = GroupServiceImpl.getInstance();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        List<Snippet> snippets = snippetService.findByGroupId(1L);
-        req.setAttribute("snippets", snippets);
+        Long id = ((Map<String, Long>) req.getServletContext().getAttribute(USER_SESSION_DATA))
+                .get(req.getSession().getId());
+        Optional<UserEntity> receivedUser = UserServiceImpl.getInstance().findById(id);
 
-        RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher(HOME);
-        dispatcher.forward(req, resp);
+        if (receivedUser.isPresent()) {
+            List<Snippet> commonSnippets = snippetService.findByGroupId(1L);
+            req.setAttribute("snippets", commonSnippets);
+
+            List<Group> userGroups = groupService.findUsersGroups(receivedUser.get().getId());
+            req.setAttribute("userGroups", userGroups);
+
+            RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher(HOME);
+            dispatcher.forward(req, resp);
+        }
     }
 }
