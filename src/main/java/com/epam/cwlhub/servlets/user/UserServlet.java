f68da6fbcf48425c47d47a928d9e6e35fdaaa42d
msleprosy <msleprosy@yandex.ru>
2019-06-05 04:30:01 +0300

    Initial version of UserBanServlet

--- src/main/java/com/epam/cwlhub/servlets/user/UserServlet.java
+++ src/main/java/com/epam/cwlhub/servlets/user/UserServlet.java
@@ -1,18 +1,45 @@
 package com.epam.cwlhub.servlets.user;
 
+import com.epam.cwlhub.entities.user.UserEntity;
+import com.epam.cwlhub.services.UserService;
+import com.epam.cwlhub.services.impl.UserServiceImpl;
+
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Optional;
 
 public class UserServlet extends HttpServlet {
-    @Override
+
+private final UserService userService = UserServiceImpl.getInstance();
+
+/*    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+        req.setAttribute("name", "userServlet");
+        req.getRequestDispatcher("/views/userView.jsp").forward(req, resp);
 
-        PrintWriter out = resp.getWriter();
-        out.print("<h1>Hello Servlet</h1>");
+    }*/
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            if (req.getParameterMap().containsKey("id")) {
+                Long id = Long.parseLong(req.getParameter("id"));
+                Optional<UserEntity> receivedUser = userService.findById(id);
+                if (receivedUser.isPresent()) {
+                    UserEntity user = receivedUser.get();
+                    if (req.getParameter("button").equals("true")) {
+                    user.setBanned(true);
+                }
+                    else {
+                        user.setBanned(false);
+                    }
+                }
+            }
+
     }
 }
---
+++ src/main/webapp/views/userView.jsp
@@ -0,0 +1,15 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: veronika
+  Date: 04.06.2019
+  Time: 19:45
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+</head>
+<body>
+<h1>Hello ${name}</h1>
+</body>
+</html>
