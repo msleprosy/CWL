9a4b8c917712c4aef34435d38ba3417a92bc2243
soignera <askasoignera@gmail.com>
2019-06-08 18:56:21 +0300

    add AdminFilter with error jsp

--- src/main/java/com/epam/cwlhub/constants/Endpoints.java
+++ src/main/java/com/epam/cwlhub/constants/Endpoints.java
@@ -1,10 +1,13 @@
 package com.epam.cwlhub.constants;
 
 public abstract class Endpoints {
+
     public static final String LOGIN_PAGE = "/loginView.jsp";
     public static final String REGISTRATION_PAGE = "/userregister.jsp";
     public static final String USERINFO_URL = "/userInfo";
     public static final String LOGIN_URL = "/login";
+    public static final String ADMIN_PAGE_URL = "/admin";
+    public static final String ADMIN_PAGE = "/views/admin.jsp";
     public static final String HOME = "/views/home.jsp";
     public static final String HOME_URL = "/home";
     public static final String SNIPPET_VIEW_URL = "/snippets";
@@ -15,6 +18,8 @@ public abstract class Endpoints {
     public static final String DELETE_SNIPPET_URL = "/delete";
     public static final String GROUP = "/views/groupContent.jsp";
     public static final String GROUP_URL = "/groups";
+    public static final String LOGOUT = "/logout";
     public static final String USERINFOVIEW_PAGE = "/UserInfoView.jsp";
     public static final String USERDETAILS ="/userdetails.jsp";
+    public static final String ERROR_PAGE = "/error.jsp";
 }
